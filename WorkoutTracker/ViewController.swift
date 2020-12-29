5c401ad1e987510b480efb2211e5a89149f50690
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-29 21:15:23 +0100

    Initial Commit

---
+++ WorkoutTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutTracker
+//
+//  Created by Lucas Goldner on 29.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
