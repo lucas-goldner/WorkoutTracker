e69736d264a7050351e28a8f49be4e8e4c9a02f3
lucas-goldner <lucas.goldner@googlemail.com>
2021-01-01 14:52:56 +0100

    New Layout

--- WorkoutTracker/SwiftUIViews/StartWorkoutView.swift
+++ WorkoutTracker/SwiftUIViews/StartWorkoutView.swift
@@ -4,6 +4,18 @@
 //
 //  Created by Lucas Goldner on 30.12.20.
 //
+//
+//                                Button(action: {
+//                                    self.contentOffset = CGPoint(x: 0, y: 100)
+//                                }) {
+//                                    Text("scroll to bottom")
+//                                }
+//
+//                                Button(action: {
+//                                    self.contentOffset = CGPoint(x: 0, y: 0)
+//                                }) {
+//                                    Text("scroll to top")
+//                                }
 
 import SwiftUI
 
@@ -23,49 +35,36 @@ struct StartWorkoutView: View {
                 
                 ScrollableView(self.$contentOffset, animationDuration: 0.5) {
                     VStack(spacing: 5) {
-                            VStack {
-                                HStack(alignment: .center, spacing: -250) {
-                                    Spacer()
-                                    VStack {
-                                        ForEach(0 ..< Workout.count) { value in
-                                            Text((workout[value] as! Exercise).name).foregroundColor(.white)
+                        VStack {
+                            VStack(spacing: 500) {
+                                ForEach(0 ..< Workout.count) { value in
+                                    VStack(spacing: 30){
+                                        HStack(alignment: .firstTextBaseline, spacing: 0) {
+                                            VStack(spacing: 20) {
+                                                Text((workout[value] as! Exercise).name).font(.system(size: 60)).foregroundColor(.white)
+                                                Text((workout[value] as! Exercise).name).foregroundColor(.white)
+                                            }
+                                            VStack(spacing: -50){
+                                                Button(action: {print("question")
+                                                }) {
+                                                    Image("question").scaleEffect(CGSize(width: 0.4, height: 0.4))
+                                                }
+                                                Button(action: {
+                                                    print("cancel")
+                                                }) {
+                                                    Image("cancel").scaleEffect(CGSize(width: 0.2, height: 0.2)).colorInvert()
+                                                }
+                                                
+                                            }.padding(.trailing, -110)
                                         }
+                                        Image("workout").cornerRadius(20).scaleEffect(CGSize(width: 1.3, height: 1.3))
                                     }
-                                    Spacer()
-                                    Spacer()
-                                    VStack{
-                                        Button(action: {
-                                           print("question")
-                                        }) {
-                                            Image("question").scaleEffect(CGSize(width: 0.4, height: 0.4))
-                                        }
-                                        Button(action: {
-                                           print("cancel")
-                                        }) {
-                                            Image("cancel").scaleEffect(CGSize(width: 0.2, height: 0.2)).colorInvert()
-                                        }
-                                      
-                                    }
-                                    
                                 }
                                 
-                                
-                                Image("workout")
-                                Button(action: {
-                                    self.contentOffset = CGPoint(x: 0, y: 100)
-                                }) {
-                                    Text("scroll to bottom")
-                                }
-                               
-                                Button(action: {
-                                    self.contentOffset = CGPoint(x: 0, y: 0)
-                                }) {
-                                    Text("scroll to top")
-                                }
                             }
+                        }
                     }
                 }
-               
             }
         }
     }
@@ -74,7 +73,7 @@ struct StartWorkoutView: View {
 struct StartWorkoutView_Previews: PreviewProvider {
     static var previews: some View {
         StartWorkoutView(workout: [Exercise(name: "Pull-up", reps: 10, time: 45),
-        Exercise(name: "Push-up", reps: 10, time: 30),
-        Exercise(name: "Sit-up", reps: 20, time: 60),])
+                                   Exercise(name: "Push-up", reps: 10, time: 30),
+                                   Exercise(name: "Sit-up", reps: 20, time: 60),])
     }
 }
